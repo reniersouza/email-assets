3144621fe78f6f79573c35a9fd48237605c4c12d
Renier Monteiro de Souza <renier@objetivonet.com.br>
2026-07-28 01:00:40 -0400

    Ajuste demo

--- assets/js/data/demo-signature.js
+++ assets/js/data/demo-signature.js
@@ -12,63 +12,63 @@
 
 export const demoSignature = {
 
-    person: {
-  
-      name:
-        'Ana Silva',
-  
-      role:
-        'Head de Operações',
-  
-      department:
-        'Atendimento',
-  
-      email:
-        'ana@objetivonet.com.br',
-  
-      phone:
-        '+55 11 3000-0000',
-  
-      whatsapp:
-        '+55 11 90000-0000'
-  
-    },
-  
-  
-    company: {
-  
-      name:
-        'ObjetivoNET',
-  
-      website:
-        'https://objetivonet.com.br',
-  
-      address:
-        'Rua Exemplo, 100',
-  
-      city:
-        'São Paulo',
-  
-      country:
-        'Brasil'
-  
-    },
-  
-  
-    photo: {
-  
-      enabled:
-        false,
-  
-      url:
-        '',
-  
-      alt:
-        '',
-  
-      size:
-        96
-  
-    }
-  
-  };
+  person: {
+
+    name:
+      'Ana Silva',
+
+    role:
+      'Head de Operações',
+
+    department:
+      'Atendimento',
+
+    email:
+      'ana@objetivonet.com.br',
+
+    phone:
+      '+55 11 3000-0000',
+
+    whatsapp:
+      ''
+
+  },
+
+
+  company: {
+
+    name:
+      'ObjetivoNET',
+
+    website:
+      'https://objetivonet.com.br',
+
+    address:
+      '',
+
+    city:
+      '',
+
+    country:
+      ''
+
+  },
+
+
+  photo: {
+
+    enabled:
+      false,
+
+    url:
+      '',
+
+    alt:
+      '',
+
+    size:
+      96
+
+  }
+
+};
